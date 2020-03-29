3810fc6c6743105709dd5b0fa116c0f39abb9dee
AlpJQ <2951258070@qq.com>
2020-03-29 09:55:17 +0800

    帖子详情功能

--- src/main/java/com/nowcoder/communityy/service/UserService.java
+++ src/main/java/com/nowcoder/communityy/service/UserService.java
@@ -1,17 +1,192 @@
 package com.nowcoder.communityy.service;
 
+import com.nowcoder.communityy.dao.LoginTicketMapper;
 import com.nowcoder.communityy.dao.UserMapper;
+import com.nowcoder.communityy.entity.LoginTicket;
 import com.nowcoder.communityy.entity.User;
+import com.nowcoder.communityy.util.CommunityConstant;
+import com.nowcoder.communityy.util.CommunityUtil;
+import com.nowcoder.communityy.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;//项目根路径
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
+    }
+
+    /**
+     * 处理前端传回来的注册的user信息
+     * @param user
+     * @return
+     */
+    public Map<String, Object> register(User user) {
+        //map存放出错信息
+        Map<String, Object> map = new HashMap<>();
+
+        //空值处理
+        if(user == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空!");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空!");
+            return map;
+        }
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空!");
+            return map;
+        }
+
+        // 验证账号
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "该账号已存在!");
+            return map;
+        }
+
+        // 验证邮箱
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "该邮箱已被注册!");
+            return map;
+        }
+
+        // 注册用户，将注册的数据写入数据库
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        /**
+         * type 0-普通用户; 1-超级管理员; 2-版主;
+         * status 0-未激活; 1-已激活;
+         */
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());//激活码
+        //使用牛客网的随机头像，有1~1000个
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+
+        // 激活邮件，使用thymeleaf模板引擎的context类对象
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        // http://localhost:8080/community/activation/101/code
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        //将设置好的模板引擎传递给动态网页activation.html
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+        return map;//如果最后注册用户信息没有问题，就会返回的map为null
+    }
+
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) { //已经激活了，返回重复激活
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(code)) { // 激活码匹配，激活成功
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else { // 激活失败
+            return ACTIVATION_FAILURE;
+        }
+    }
+
+
+    /*
+        登录
+     */
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+
+        // 空值处理
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空!");
+            return map;
+        }
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空!");
+            return map;
+        }
+
+        // 验证账号
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            map.put("usernameMsg", "该账号不存在!");
+            return map;
+        }
+
+        // 验证状态
+        if (user.getStatus() == 0) {
+            map.put("usernameMsg", "该账号未激活!");
+            return map;
+        }
+
+        // 验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(password)) {
+            map.put("passwordMsg", "密码不正确!");
+            return map;
+        }
+
+        // 生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
+
+    /*
+        退出
+     */
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket, 1);
+    }
+
+    public LoginTicket findLoginTicket(String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+    //更新头像路径
+    public int updateHeader(int userId, String headerUrl) {
+        return userMapper.updateHeader(userId, headerUrl);
     }
 }
--- src/test/java/com/nowcoder/communityy/MapperTests.java
+++ src/test/java/com/nowcoder/communityy/MapperTests.java
@@ -1,8 +1,10 @@
 package com.nowcoder.communityy;
 
 import com.nowcoder.communityy.dao.DiscussPostMapper;
+import com.nowcoder.communityy.dao.LoginTicketMapper;
 import com.nowcoder.communityy.dao.UserMapper;
 import com.nowcoder.communityy.entity.DiscussPost;
+import com.nowcoder.communityy.entity.LoginTicket;
 import com.nowcoder.communityy.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +26,10 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
 
     @Test
     public void testSelectUser() {
@@ -74,4 +80,27 @@ public class MapperTests {
         int row = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(row);
     }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
+
 }
